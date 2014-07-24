94e4466f4c94729a6b8ee241a108eb10208abbdb
VodkaBears <dfrost.00@gmail.com>
2014-07-24 05:50:32 +0400

    Add extended styles for slides

--- app/js/app.js
+++ app/js/app.js
@@ -5,23 +5,23 @@
         $scope.slides = [{
             title: 'Slide 1',
             content: 'This is slide №1',
-            background: '#e74c3c'
+            style: {'background': '#e74c3c'}
         }, {
             title: 'Slide 2',
             content: 'This is slide №2',
-            background: '#e67e22'
+            style: {'background': '#e67e22'}
         }, {
             title: 'Slide 3',
             content: 'This is slide №3',
-            background: '#1abc9c'
+            style: {'background': '#1abc9c'}
         }, {
             title: 'Slide 4',
             content: 'This is slide №4',
-            background: '#3498db'
+            style: {'background': '#3498db'}
         }, {
             title: 'Slide 5',
             content: 'This is slide №5',
-            background: '#9b59b6'
+            style: {'background': '#9b59b6'}
         }];
     });
 })(angular);
